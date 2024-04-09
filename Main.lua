b0256400a89b8bb3378bac4b166540361814b941
Papubaras <111273482+Chorritoh@users.noreply.github.com>
2024-04-08 23:54:58 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -22,7 +22,8 @@ local GameId = game.PlaceId
 -- GameScripts
 local GameScripts = {
     [1686885941] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/BrookHaven.lua",
-    [14315258385] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/RSS-3.lua"
+    [14315258385] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/RSS-3.lua",
+    [5307215810] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/Randomizer.Lua"
     -- Add more here...
 }
 

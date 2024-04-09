1ad27617b966c0bfd17c5cfc3bf1d81241edfa6b
Papubaras <111273482+Chorritoh@users.noreply.github.com>
2024-04-08 22:14:16 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,17 @@
-idk
+-- GameId
+local GameId = game.PlaceId
+
+-- GameScripts
+local GameScripts = {
+    [1686885941] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/BrookHaven.lua",
+    [14315258385] = "https://raw.githubusercontent.com/Chorritoh/KapybarasHub/main/BrookHaven.lua"
+    -- Add more here...
+}
+
+-- Check if the GameId is in the GameScripts table
+if GameScripts[GameId] then
+    -- If so, execute the corresponding loadstring
+    loadstring(game:HttpGet(GameScripts[GameId]))()
+else
+    print("The GameId does not match any of the specified ones.")
+end
